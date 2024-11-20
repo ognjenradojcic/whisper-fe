2856c03195b7c2aa0012e7695cb791a2f3d740b3
Ognjen Radojcic <oradojccic@yahoo.com>
2024-11-20 13:47:11 +0100

    Use new PrivateChannel broadcasting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
-import Echo from 'laravel-echo'
+import axios from 'axios';
 
 function App() {
   const [username, setUsername] = useState('username')
@@ -9,19 +10,65 @@ function App() {
   const [message, setMessage] = useState('')
   let allMessages = [];
 
-
   useEffect(() => {
-    Pusher.logToConsole = true;
+    // Pusher.logToConsole = true;
 
-    const pusher = new Pusher('f459af705400ac2b079e', {
-      cluster: 'eu'
+    // const pusher = new Pusher('f459af705400ac2b079e', {
+    //   cluster: 'eu'
+    // });
+
+
+
+    window.Echo = new Echo({
+      authEndpoint: 'http://localhost:8000/api/broadcasting/auth',
+      broadcaster: 'pusher',
+      key: 'f459af705400ac2b079e',
+      cluster: 'eu',
+      encrypted: true,
+      authorizer: (channel, options) => {
+        return {
+          authorize: (socketId, callback) => {
+            axios.post('http://localhost:8000/api/broadcasting/auth',
+              {
+                socket_id: socketId,
+                channel_name: channel.name
+              },
+              {
+                withCredentials: true,
+                headers: {
+                  Accept: 'application/json',
+                  Authorization: 'Bearer 2|0jmkmqEMI7JTEXYK4MWT6wACAJ8jUjkGJVgIAMwq1c57ae6a'
+                },
+              }
+            )
+              .then(response => {
+                callback(false, response.data);
+              })
+              .catch(error => {
+                callback(true, error);
+              });
+          }
+        };
+      },
+      // auth: {
+      //   headers: {
+      //     authorization: 'Bearer 2|0jmkmqEMI7JTEXYK4MWT6wACAJ8jUjkGJVgIAMwq1c57ae6a'
+      //   }
+      // }
     });
 
-    const channel = pusher.subscribe('chat');
+    window.Echo.private('chat')
+      .listen('MessageReceived', (e) => {
+        allMessages.push(e);
+        setMessages(allMessages)
+      })
+
+    /*const channel = pusher.subscribe('chat');
     channel.bind('message', function (data) {
       allMessages.push(data);
       setMessages(allMessages)
     });
+    */
   }, [])
 
   const submit = async e => {
@@ -48,8 +95,8 @@ function App() {
         <div className="list-group list-group-flush border-bottom scrollarea">
           <div>
           </div>
-          {messages.map(message => (
-            <div className="list-group-item list-group-item-action py-3 lh-sm" >
+          {messages.map((message, index) => (
+            <div className="list-group-item list-group-item-action py-3 lh-sm" key={index}>
               <div className="d-flex w-100 align-items-center justify-content-between">
                 <strong className="mb-1">{message.username}</strong>
               </div>
